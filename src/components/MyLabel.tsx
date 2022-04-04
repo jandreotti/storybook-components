90ae2e70a16c17a74508c01d2930971003ca7cb5
jandreotti <joa.andreotti@gmail.com>
2022-04-04 11:09:09 -0300

    message

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './mylabel.css';
+
+interface MyLabelProps {
+	/**
+	 * El texto que va a figurar en el label
+	 */
+	label?: string;
+
+	/**
+	 * El tamaño del label
+	 */
+	size?: 'normal' | 'h1' | 'h2' | 'h3';
+
+	/**
+	 * Transformar el texto todo a mayusculas
+	 */
+	allCaps?: boolean;
+
+	/**
+	 * Color del texto
+	 */
+	color?: 'primary' | 'secondary' | 'tertiary';
+
+	/**
+	 * Color del texto personalizado
+	 */
+	fontColor?: string;
+
+	/**
+	 * Color del fondo
+	 */
+	backgroundColor?: string;
+}
+
+export const MyLabel = ({
+	label = 'No Label', // con esto lo que hago es que si no se pasa el label, le asigno un valor por defecto pero NO FIGURA EN EL STORYBOOK COMO VALOR SETEADO
+	size = 'normal',
+	allCaps = false,
+	color = 'primary',
+	fontColor = '',
+	backgroundColor = 'transparent',
+	...props
+}: MyLabelProps) => {
+	return (
+		<span className={`label ${size} text-${color}`} {...props} style={{ color: fontColor, backgroundColor }}>
+			{allCaps ? label.toUpperCase() : label}
+		</span>
+	);
+};
---
+++ src/index.tsx
@@ -0,0 +1,3 @@
+//! Esto solo lo hago para evitar el error de typescript
+export { Button } from "./stories/Button";
+
